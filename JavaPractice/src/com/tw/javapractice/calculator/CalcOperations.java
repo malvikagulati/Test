faca4b90b056c77d9780c29e83c710c5cc9b930b
malvika <malvikagulati1983@gmail.com>
2021-11-13 18:58:51 +0530

    Day 2 Assignments

---
+++ JavaPractice/src/com/tw/javapractice/calculator/CalcOperations.java
@@ -0,0 +1,8 @@
+package com.tw.javapractice.calculator;
+
+public class CalcOperations {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        cal.doOperations();
+    }
+}

1d01376b0d719aa0abf3d6c26a12068dfd1c0786
malvika <malvikagulati1983@gmail.com>
2021-11-21 18:31:58 +0530

    Commit 2 and commit 3

--- VendingMachine/src/com/tw/main/VendingMachineMain.java
+++ VendingMachine/src/com/tw/main/VendingMachineMain.java
@@ -7,23 +7,29 @@ import java.util.Scanner;
 
 public class VendingMachineMain {
     public static void main(String[] args) {
-        int itemIdCounter=1;
+        int itemIdCounter = 1;
         VendingMachine vendingMachine = new VendingMachine("Vending Machine1");
-        Product coldDrink =  new Product(itemIdCounter++,"Pepsi", 20, 3 );
-        Product biscuits =  new Product(itemIdCounter++,"Parle-G", 5, 5 );
-        Product namkeen =  new Product(itemIdCounter++,"Bhujia", 10, 10 );
+        Product coldDrink = new Product(itemIdCounter++, "Pepsi", 20, 3);
+        Product biscuits = new Product(itemIdCounter++, "Parle-G", 5, 5);
+        Product namkeen = new Product(itemIdCounter++, "Bhujia", 10, 10);
         vendingMachine.addProduct(coldDrink);
         vendingMachine.addProduct(biscuits);
         vendingMachine.addProduct(namkeen);
-
-        vendingMachine.displayProducts();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Item-Id to purchase");
-        int itemId = sc.nextInt();
-        System.out.println("Enter Amount:");
-        double amount = sc.nextDouble();
-
-        vendingMachine.sellProduct(itemId, amount);
-        System.out.println("Total Amount in Vending Machine is:"+vendingMachine.getTotalAmount());
+        while (true) {
+            vendingMachine.displayProducts();
+            System.out.println("Enter Item-Id to purchase");
+            int itemId = sc.nextInt();
+            System.out.println("Enter Amount:");
+            double amount = sc.nextDouble();
+            vendingMachine.sellProduct(itemId, amount);
+            System.out.println("Do yo want to continue? Enter y or n.");
+            String response = sc.next();
+            if (response.equalsIgnoreCase("y"))
+                continue;
+            else
+                break;
+        }
+        System.out.println("Total Amount in Vending Machine is:" + vendingMachine.getTotalAmount());
     }
 }
